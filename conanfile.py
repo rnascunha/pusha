cc14a2e6c43f9ad7a009afc55359a9621119ef7c
Rafael Cunha <rnascunha@gmail.com>
2022-09-30 15:19:13 -0300

    Adding conan recipe.

--- conanfile.py
+++ conanfile.py
@@ -9,11 +9,12 @@ class PushaConan(ConanFile):
     description = "C/C++ implementation of Web Push"
     topics = ("web", "web push", "notification")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [True, False], "fPIC": [True, False], "tools": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "tools": False}
     generators = "cmake_find_package"
     exports = "LICENSE", "README.md", "URL.txt"
-    exports_sources = "*.c", "*.cpp", "*.h", "*.hpp", "*.cmake", "CMakeLists.txt"
+    exports_sources = "third/*", "*.c", "*.cpp", "*.h", "*.hpp", "*.cmake", "CMakeLists.txt"
+    requires = "openssl/1.1.1f"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -21,14 +22,15 @@ class PushaConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["WITH_TOOLS"] = self.options.tools
         cmake.configure()
         cmake.build()
 
     def package(self):
         self.copy("*.h", dst="include", src="src")
-        self.copy("*.hpp", dst="include", src="src")
+        self.copy("*.hpp", dst="include_cpp", src="src")
         self.copy("*.c", dst="src", src="src")
-        self.copy("*.cpp", dst="src", src="src")
+        self.copy("*.cpp", dst="src", src="src_cpp")
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
